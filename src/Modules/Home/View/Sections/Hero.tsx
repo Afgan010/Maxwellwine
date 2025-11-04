21828c1a0dbb777638613481339a2f147a36fd77
Afgan010 <afganmammadzada010@gmail.com>
2025-11-04 22:06:39 +0400

    Hero section added

--- src/Modules/Home/View/Sections/Hero.tsx
+++ src/Modules/Home/View/Sections/Hero.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import video from "../../../../assets/videos/Hero-video.mp4";
 
 const Hero = () => {
@@ -11,6 +12,28 @@ const Hero = () => {
         playsInline
         className="bgVideo"
       ></video>
+      <div className="container">
+        <div className="row">
+          <div className="rightSide">
+            <h2 className="title">
+              GROUNDED IN <br />
+              LIMESTONE & LEGACY
+            </h2>
+            <p className="text">
+              Rooted in the heart of McLaren Vale, Maxwell Wines is a <br />
+              family-owned estate where tradition meets innovation.
+            </p>
+          </div>
+          <div className="leftSide">
+            <Link to="/about" className="btn aboutBtn">
+              About Us
+            </Link>
+            <Link to="/shop" className="btn">
+              Shop Now
+            </Link>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
